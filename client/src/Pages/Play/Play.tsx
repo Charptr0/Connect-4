aef3174bb1131c384e0561ca37355192147c9011
Charptr0 <CharlesOfficial1@hotmail.com>
2022-07-30 21:49:50 -0400

    added socket and moved board logic to play page

--- client/src/Pages/Play/Play.tsx
+++ client/src/Pages/Play/Play.tsx
@@ -1,11 +1,63 @@
+import { useEffect, useState } from "react";
 import Board from "./Components/Board";
 import styles from "./Play.module.css";
+import { io } from "socket.io-client";
+import { SocketProvider } from "./Context/SocketContext";
+import { Cell, checkWinner, initializeBoard } from "./Utils";
+
+const socket = io("http://localhost:4000");
 
 export default function Play() : JSX.Element 
 {
-    // 7 x 6
+    const [currentBoard, setCurrentBoard] = useState<number[][]>(initializeBoard());
+    const [isPlayer1Turn, setPlayer1Turn] = useState<boolean>(true);
+
+    useEffect(() => {    
+        socket.on("connect", () => {
+            console.log(`connected with id ${socket.id}`);
+        });
+    }, []);
+
+    /**
+     * Update the board and send the request to the other player
+     * 
+     * @param col the column number that the user has clicked
+     */
+    function onPlayerMove(col : number)
+    {
+        // prevent the user from making a move when a column is full
+        if(currentBoard[0][col] !== Cell.EMPTY) {
+            console.log("Invalid placement");
+            return;
+        }
+
+        // search from bottom to top
+        // we will place the player in the first empty cell
+        for(let i = currentBoard.length - 1; i >= 0; i--) {
+            const cell = currentBoard[i][col];
+
+            // cell is found empty
+            if(cell === Cell.EMPTY) {
+                // update board
+                isPlayer1Turn ? currentBoard[i][col] = Cell.PLAYER_1 : currentBoard[i][col] = Cell.PLAYER_2;
+                setCurrentBoard(currentBoard);
+
+                // switch turns
+                setPlayer1Turn(!isPlayer1Turn);
+                break;
+            }
+        }
+        
+        if(checkWinner(currentBoard) !== Cell.EMPTY) {
+            console.log("a player wins");
+        }
+    }
+
     return (
-    <div className={styles.flexContainer}>
-        <Board />
-    </div>)
+        <SocketProvider>
+            <div className={styles.flexContainer}>
+                <Board board={currentBoard}  isPlayer1Turn={isPlayer1Turn} onPlayerMoveHandler={onPlayerMove}/>
+            </div>
+        </SocketProvider>
+    )
 }
